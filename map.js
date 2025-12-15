ef4e782b6adac921fa8ad5debd733b68ab59c6e8
farizwebdev <farizabar80@gmail.com>
2025-12-15 09:51:51 +0700

    ubah map

--- map.js
+++ map.js
@@ -1,7 +1,17 @@
+//Buat Map
 const map = new Map();
+
+//Tambahin properti nama
 map.set('nama', 'fariz');
+
+//Tambahin properti hobi
 map.set('hobi', 'jogging');
+
+//Akses properti nama
 console.log(map.get('nama'));
 
+//Hapus properti Hobi
 map.delete("hobi");
+
+//Nampilin Map setelah properti hobi dihapus
 console.log(map);
