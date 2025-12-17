9e5d9822f4d7aeb0e50c81230ed8e10568d479e3
farizwebdev <farizabar80@gmail.com>
2025-12-17 17:20:55 +0700

    ubah

--- if_statement.js
+++ if_statement.js
@@ -2,12 +2,16 @@ const anak_yuin = true;
 
 console.log("Yeay, aku anak Yuin!")
 
+
+// Contoh else
 if (anak_yuin) {
     console.log("Sanaa, sholattt duluu!");
 } else {
     console.log("Aku anak Yuin kok bukan anak Budi");
 };
 
+
+// Contoh else if
 const nilai_tbo = 78;
 
 if (nilai_tbo >= 89){
