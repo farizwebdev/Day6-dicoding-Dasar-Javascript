be210065848483acabb97cd21d67db8133e66e86
farizwebdev <farizabar80@gmail.com>
2025-12-18 03:27:44 +0700

    ubah

---
+++ for_in.js
@@ -0,0 +1,9 @@
+const istri_Fariz = {
+    nama: 'Hesti Febriani',
+    hobi: 'Gombalin Fariz',
+    umur: 19,
+}
+
+for (const panggil in istri_Fariz){
+    console.log(`Tolong kamu siapaa?? ${istri_Fariz[panggil]}`);
+};
