7244e9f01491d992a312755dbd99dbf4eb621efe
farizwebdev <farizabar80@gmail.com>
2025-12-17 17:16:59 +0700

    ubah

--- if_statement.js
+++ if_statement.js
@@ -7,3 +7,13 @@ if (anak_yuin) {
 } else {
     console.log("Aku anak Yuin kok bukan anak Budi");
 };
+
+const nilai_tbo = 78;
+
+if (nilai_tbo >= 89){
+    console.log("Yeayy, Nilai A");
+} else if (nilai_tbo >= 79){
+    console.log("Waduh, Nilai B");
+} else {
+    console.log("Sayang kali kau dapet nilai C");
+};
