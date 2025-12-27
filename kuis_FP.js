64b8ba59981d3d4c40d199e46f7ab9601f6619dc
farizwebdev <farizabar80@gmail.com>
2025-12-27 08:41:08 +0700

    ubah

--- kuis_FP.js
+++ kuis_FP.js
@@ -48,6 +48,6 @@ function applyDiscount(products, discount) {
 }
 
 console.log(getProductsByCategory(sampleProducts, 'Electronics')); // cetak produk dalam kategori 'Electronics'
-console.log(calculateTotalPrice(sampleProducts)); // Should return 1830
+console.log(calculateTotalPrice(sampleProducts)); // cetak total harga semua produk
 console.log(applyDiscount(sampleProducts, 10)); // Should return products with prices reduced by 10%
 console.log(findProductById(sampleProducts, 3)); // Should return the product with id 3
